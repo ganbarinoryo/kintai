0b893bfb30db51332b13d62460579fdeae7d49e4
ryo morita <epkvg5316@gmail.com>
2024-10-30 21:29:07 +0900

    ページ遷移、認証、データ送信、ボタンの制御80%、CSS調節

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -1,17 +1,50 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('css')
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Atte</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/attendance.css') }}" />
 </head>
-<link rel="stylesheet" href="{{ asset('css/attendance.css') }}">
-<link rel="stylesheet" href="{{ asset('css/common.css') }}" />
-@endsection
 
-@section('content')
+<body>
+        <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">Atte</a>
+
+<!--ヘッダー右側のリンク３つ-->
+          <ul class="flex__header-nav">
+            @if (Auth::check())
+            <li class="header-nav__item">
+                <a href="/">ホーム</a>
+            </li>
+            <li class="header-nav__item">
+                <form action="/attendance" method="get">
+                    @csrf
+                    <button class="header-nav__button">日付一覧</button>
+                </form>
+            </li>
+            <li class="header-nav__item">
+                <form action="/logout" method="POST">
+                    @csrf
+                    <button class="header-nav__button">ログアウト</button>
+                </form>
+            </li>
+            @endif
+          </ul>
+
+</div>
+</header>
+
+<main>
 
 <div class="attendance__content">
     <div class="attendance-content__heading">
-        <h2>福場凛太郎さんお疲れ様です！</h2>
+        <h2>山田太郎さんお疲れ様です！</h2>
     </div>
 
 <!--データテーブル-->
@@ -39,4 +72,14 @@
             </table>
             <div class="flex__pagination">{{ $users->links('pagination::Bootstrap-4') }}</div>
         </div>
-@endsection
+
+</main>
+
+<footer class="footer">
+    <div class="footer__inner">
+        <h5>Atte, inc.</h5>
+    </div>
+</footer>
+
+</body>
+</html>
